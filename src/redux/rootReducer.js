e26b18abc10404831a5fca7cc69935102cd9d852
sivlvy <vladislavsidenko@ukr.net>
2024-04-23 12:37:16 +0300

    testing

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,21 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
 
 import contactsReducer from './contacts/contacts-slice';
 import filterReducer from './filter/filter-slice';
-
-const persistConfig = {
-	key: 'contacts',
-	storage,
-	whitelist: ['contacts'],
-};
 
 const rootReducer = combineReducers({
 	contacts: contactsReducer,
 	filter: filterReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-export default persistedReducer;
+export default rootReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import {
-	persistStore,
-	FLUSH,
-	REHYDRATE,
-	PAUSE,
-	PERSIST,
-	PURGE,
-	REGISTER,
-} from 'redux-persist';
-
-import persistedReducer from './rootReducer';
+import rootReducer from './rootReducer';
 
 export const store = configureStore({
-	reducer: persistedReducer,
-	middleware: getDefaultMiddleware =>
-		getDefaultMiddleware({
-			serializableCheck: {
-				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-			},
-		}),
+	reducer: rootReducer,
 });
-
-export const persistor = persistStore(store);
